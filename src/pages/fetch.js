b09da13ac9aa70e1e66139addc8bccfa6f833c62
Jason Johnson <jas2johns@gmail.com>
2023-02-14 13:14:52 -0800

    initialized puppDex

---
+++ src/pages/fetch.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import React from "react";
+
+// const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+}
