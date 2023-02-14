2ae7bf521fb1ab23c0daa7119bf6c078ffb8c684
Jason Johnson <jas2johns@gmail.com>
2023-02-14 15:19:19 -0800

    tinkered with fetch setup; dog groups & colors est

--- src/pages/fetch.js
+++ src/pages/fetch.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 
 // const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://thedogapi.com/";
 
 export default function App() {
   const [post, setPost] = React.useState(null);
@@ -21,3 +22,41 @@ export default function App() {
     </div>
   );
 }
+
+
+// template !!!
+
+const fetchData = async searchTerm => {
+    const response = await axios.get('https://thedogapi.com/', {
+      params: {
+        // apikey: 'd9835cc5',
+        // dogAPI Key
+        apikey: 'live_gPOoAfRbgovE80W0XvqyidYXNAhOaM0brZAye65mJ4nHiiMwk2bBedxS7UxZA5m1',
+        s: searchTerm
+      }
+    });
+  
+    if (response.data.Error) {
+      return [];
+    }
+  
+    return response.data.Search;
+  };
+  
+  const input = document.querySelector('input');
+  
+  const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+  
+    for (let movie of movies) {
+      const div = document.createElement('div');
+  
+      div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+      `;
+  
+      document.querySelector('#target').appendChild(div);
+    }
+  };
+  input.addEventListener('input', debounce(onInput, 500));
