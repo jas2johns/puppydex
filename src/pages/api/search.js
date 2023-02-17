4341574fdd1678c71ad4a3f62809f0f190d7d433
Jason Johnson <jas2johns@gmail.com>
2023-02-17 15:47:27 -0800

    fixed axios

---
+++ src/pages/api/search.js
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import axios from 'axios';
+
+const baseUrl = 'https://api.thedogapi.com/';
+
+export default async function handler(req, res) {
+  // read the query from the request
+  const response = await axios
+			.get(baseUrl + `v1/breeds/search?limit=10&page=0&q=terrier`, {
+				headers: {
+					'x-api-key': 'live_gPOoAfRbgovE80W0XvqyidYXNAhOaM0brZAye65mJ4nHiiMwk2bBedxS7UxZA5m1'
+				}
+			});
+			const firstBreed = response.data[0];
+      const imageId = firstBreed.reference_image_id;
+      const imageResponse = await axios.get(baseUrl + `v1/images/${imageId}`);
+      const imageUrl = imageResponse.data.url;
+      const breedInfo = {...firstBreed, imageUrl};
+
+				console.log(response.data);
+			// map over the array of breeds and get the images and some stats
+      
+        res.status(200).json(breedInfo);
+}
--- src/pages/fetch.js
+++ src/pages/fetch.js
@@ -1,62 +1,49 @@
-import axios from "axios";
-import React from "react";
+import axios from 'axios';
+import React from 'react';
 
 // const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
-const baseURL = "https://thedogapi.com/";
 
 export default function App() {
-  const [post, setPost] = React.useState(null);
+	React.useEffect(() => {
+		axios.get(`/api/search`).then(response => console.log(response.data))
+	}, []);
 
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
-
-  return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  );
+	return <div />;
 }
-
 
 // template !!!
 
-const fetchData = async searchTerm => {
-    const response = await axios.get('https://thedogapi.com/', {
-      params: {
-        // apikey: 'd9835cc5',
-        // dogAPI Key
-        apikey: 'live_gPOoAfRbgovE80W0XvqyidYXNAhOaM0brZAye65mJ4nHiiMwk2bBedxS7UxZA5m1',
-        s: searchTerm
-      }
-    });
-  
-    if (response.data.Error) {
-      return [];
-    }
-  
-    return response.data.Search;
-  };
-  
-  const input = document.querySelector('input');
-  
-  const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-  
-    for (let movie of movies) {
-      const div = document.createElement('div');
-  
-      div.innerHTML = `
-        <img src="${movie.Poster}" />
-        <h1>${movie.Title}</h1>
-      `;
-  
-      document.querySelector('#target').appendChild(div);
-    }
-  };
-  input.addEventListener('input', debounce(onInput, 500));
+// const fetchData = async searchTerm => {
+//     const response = await axios.get('https://thedogapi.com/', {
+//       params: {
+//         // apikey: 'd9835cc5',
+//         // dogAPI Key
+//         apikey: 'live_gPOoAfRbgovE80W0XvqyidYXNAhOaM0brZAye65mJ4nHiiMwk2bBedxS7UxZA5m1',
+//         s: searchTerm
+//       }
+//     });
+
+//     if (response.data.Error) {
+//       return [];
+//     }
+
+//     return response.data.Search;
+//   };
+
+//   const input = document.querySelector('input');
+
+//   const onInput = async event => {
+//     const movies = await fetchData(event.target.value);
+
+//     for (let movie of movies) {
+//       const div = document.createElement('div');
+
+//       div.innerHTML = `
+//         <img src="${movie.Poster}" />
+//         <h1>${movie.Title}</h1>
+//       `;
+
+//       document.querySelector('#target').appendChild(div);
+//     }
+//   };
+//   input.addEventListener('input', debounce(onInput, 500));
