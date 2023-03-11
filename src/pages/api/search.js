bc33b2d6e25af115dcdd47abf3cf0e2b2f20f343
Jason Johnson <jas2johns@gmail.com>
2023-03-10 16:28:56 -0800

    added notes

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -23,3 +23,8 @@ export default async function handler(req, res) {
       
         res.status(200).json(breedInfo);
 }
+
+//for the search return
+// response.data.slice(0,2)
+
+// make sure for the puppy card that you use the image tag to render the URL
