7f2ffb5bba5c4d0d7cf843c01a728fba68c67b58
Jason Johnson <jas2johns@gmail.com>
2023-04-05 10:39:55 -0700

    fixed dog groups

--- src/pages/dogGroups.js
+++ src/pages/dogGroups.js
@@ -1,11 +1,16 @@
-const dogs = {
-	sporting: '#C38E70',
-	hound: '#8A5A44',
-	working: '#B07D62',
-	terrier: '#9D6B53',
-	toy: '#E6B8A2',
-	noSporting: '#CD9777',
-	herding: '#D69F7E',
-	miscellaneousServices: '#774936',
-	foundationStockService: '#DEAB90'
-};
+export default () => {
+	const dogs = {
+	  sporting: '#C38E70',
+	  hound: '#8A5A44',
+	  working: '#B07D62',
+	  terrier: '#9D6B53',
+	  toy: '#E6B8A2',
+	  noSporting: '#CD9777',
+	  herding: '#D69F7E',
+	  miscellaneousServices: '#774936',
+	  foundationStockService: '#DEAB90'
+	};
+	
+	return dogs;
+  };
+  
