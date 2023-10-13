e5cfc58590764fb30f8d2d0a1100cf872a6c1ffb
Jon Carter <jon@niibble.com>
2023-10-13 19:15:39 -0400

    card animation sequence done

--- app/components/Carousel/index.js
+++ app/components/Carousel/index.js
@@ -1,9 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BreedCard from "../Card";
 import styles from "./Carousel.module.scss";
+import { motion, frame, useTransform, animate } from "framer-motion";
 
 const Carousel = ({ breeds }) => {
 	const [activeItemId, setActiveItemId] = useState();
+
+	const animateInactiveCard = (card) => {
+		animate(
+			card,
+			{
+				scale: 0.8,
+			},
+			{
+				duration: 0.5,
+			}
+		);
+	};
+
+	const animateActiveCard = (card) => {
+		const sequence = [
+			[
+				card,
+				{
+					scale: 1.1,
+				},
+				{
+					duration: 0.5,
+				},
+			],
+			[
+				card,
+				{
+					scale: 1,
+				},
+				{
+					duration: 0.5,
+				},
+			],
+		];
+
+		animate(sequence);
+	};
+
+	useEffect(() => {
+		const allCards = document.querySelectorAll(
+			`.${styles["carousel__item"]}`
+		);
+
+		allCards.forEach((c) => {
+			if (Number(c.id) === activeItemId) {
+				animateActiveCard(c);
+			} else {
+				animateInactiveCard(c);
+			}
+		});
+	}, [activeItemId]);
+
 	return (
 		<div className={styles["wrapper"]}>
 			<div className={styles["carousel"]}>
@@ -11,12 +64,9 @@ const Carousel = ({ breeds }) => {
 					breeds.length > 0 &&
 					breeds.map((br) => (
 						<div
+							id={br.id}
 							key={br.id}
-							className={`${styles["carousel__item"]} ${
-								activeItemId == br.id
-									? styles["carousel__item--active"]
-									: ""
-							} `}
+							className={styles["carousel__item"]}
 							onClick={() => setActiveItemId(br.id)}
 						>
 							<BreedCard breed={br} />
